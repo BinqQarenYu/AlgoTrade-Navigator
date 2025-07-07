3610837a50377b02b5bddfd3e2d8ff681c1f000e
Ben Carino <benjohncarino@gmail.com>
2025-07-07 17:14:25 +0000

    In API page, allow keyboard  to execution of ok enter to approve

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -39,6 +39,16 @@ const AlertDialogContent = React.forwardRef<
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
         className
       )}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' && !e.defaultPrevented) {
+          const actionButton = (e.currentTarget as HTMLElement).querySelector<HTMLElement>('[data-alert-dialog-action="true"]');
+          if (actionButton) {
+            e.preventDefault();
+            actionButton.click();
+          }
+        }
+        props.onKeyDown?.(e);
+      }}
       {...props}
     />
   </AlertDialogPortal>
@@ -105,6 +115,7 @@ const AlertDialogAction = React.forwardRef<
   <AlertDialogPrimitive.Action
     ref={ref}
     className={cn(buttonVariants(), className)}
+    data-alert-dialog-action="true"
     {...props}
   />
 ))
