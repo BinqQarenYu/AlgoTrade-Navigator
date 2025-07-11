43d31ef000f7704c246dc9c21be20bf308cd297e
Ben Carino <benjohncarino@gmail.com>
2025-07-11 09:24:27 +0000

    in paper trading increate the interval  (5m, 15m, 1 hr, 4hrs)

--- src/app/(app)/simulation/page.tsx
+++ src/app/(app)/simulation/page.tsx
@@ -570,9 +570,15 @@ export default function SimulationPage() {
                     </div>
                     <div>
                       <Label htmlFor="interval">Interval</Label>
-                      <Select onValueChange={setInterval} value={interval} disabled={isRunning}><SelectTrigger id="interval"><SelectValue /></SelectTrigger>
+                      <Select onValueChange={setInterval} value={interval} disabled={isRunning}>
+                        <SelectTrigger id="interval"><SelectValue /></SelectTrigger>
                         <SelectContent>
-                          <SelectItem value="1m">1m</SelectItem><SelectItem value="5m">5m</SelectItem><SelectItem value="15m">15m</SelectItem>
+                            <SelectItem value="1m">1 Minute</SelectItem>
+                            <SelectItem value="5m">5 Minutes</SelectItem>
+                            <SelectItem value="15m">15 Minutes</SelectItem>
+                            <SelectItem value="1h">1 Hour</SelectItem>
+                            <SelectItem value="4h">4 Hours</SelectItem>
+                            <SelectItem value="1d">1 Day</SelectItem>
                         </SelectContent>
                       </Select>
                     </div>
