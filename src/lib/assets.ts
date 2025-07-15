f48b265ef60fab9e3304cc0e267d9dad37c7a068
Ben Carino <benjohncarino@gmail.com>
2025-07-15 15:01:12 +0000

    include sahara usdt on the list of assets

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -17,7 +17,7 @@ export const assetInfo: Record<string, string> = {
     "ENA": "Ethena", "W": "Wormhole", "JUP": "Jupiter", "JTO": "Jito", "PYTH": "Pyth Network",
     "BOME": "BOOK OF MEME", "ICP": "Internet Computer", "VET": "VeChain", "XLM": "Stellar",
     "HBAR": "Hedera", "ALGO": "Algorand", "EGLD": "MultiversX", "FLOW": "Flow", "CHZ": "Chiliz",
-    "MINA": "Mina", "HYPER": "Hyper"
+    "MINA": "Mina", "HYPER": "Hyper", "SAHARA": "Sahara"
 };
 
 export const fullAssetList = [
@@ -28,7 +28,7 @@ export const fullAssetList = [
     "MANAUSDT", "AXSUSDT", "GALAUSDT", "THETAUSDT", "XTZUSDT", "EOSUSDT", "KSMUSDT", "ZECUSDT", "DASHUSDT", "COMPUSDT",
     "CRVUSDT", "1INCHUSDT", "DYDXUSDT", "GMXUSDT", "SUSHIUSDT", "YFIUSDT", "IMXUSDT", "BLURUSDT", "CELOUSDT", "FLOKIUSDT",
     "MEMEUSDT", "ENAUSDT", "WUSDT", "JUPUSDT", "JTOUSDT", "PYTHUSDT", "BOMEUSDT", "ICPUSDT", "VETUSDT", "XLMUSDT",
-    "HBARUSDT", "ALGOUSDT", "EGLDUSDT", "FLOWUSDT", "CHZUSDT", "MINAUSDT", "HYPERUSDT",
+    "HBARUSDT", "ALGOUSDT", "EGLDUSDT", "FLOWUSDT", "CHZUSDT", "MINAUSDT", "HYPERUSDT", "SAHARAUSDT",
     // Add some pairs with other quotes
     "ETHBTC", "BNBBTC", "SOLBTC", "XRPBTC", "ADABTC", "DOTBTC", "LINKBTC", "LTCBTC",
     "BNBETH", "TRXETH", "SOLETH",
@@ -82,7 +82,7 @@ export const pairsByBase = allPairs.reduce((acc, pair) => {
 export const topBases = [
     "BTC", "ETH", "BNB", "SOL", "XRP", "DOGE", "ADA", "AVAX", "DOT",
     "LINK", "MATIC", "LTC", "NEAR", "UNI", "ATOM", "ETC", "FIL", "APT", "SUI", "OP",
-    "PEPE", "WIF", "TON", "ORDI", "WLD", "ARB", "HYPER"
+    "PEPE", "WIF", "TON", "ORDI", "WLD", "ARB", "HYPER", "SAHARA"
 ].sort();
 
 // New export for UI components
